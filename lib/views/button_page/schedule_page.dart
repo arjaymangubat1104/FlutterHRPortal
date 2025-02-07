5cbb47dd0b32b4d77d34606a647c20ff5b66f407
arjaymangubat1104 <tititaniumbackup11@gmail.com>
2025-02-07 20:52:48 +0800

    Update schedule_page.dart
    -added app bar color background

--- lib/views/button_page/schedule_page.dart
+++ lib/views/button_page/schedule_page.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_attendance_system/viewmodel/theme_view_model.dart';
+import 'package:provider/provider.dart';
 
-class SchedulePage extends StatelessWidget{
+class SchedulePage extends StatelessWidget {
   const SchedulePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final theviewModel = Provider.of<ThemeViewModel>(context);
     return Scaffold(
       appBar: AppBar(
-        title: Text('News'),
+        backgroundColor: theviewModel.currentTheme.themeColor,
+        title: Text(
+          'Schedule',
+          style: TextStyle(
+            color: theviewModel.currentTheme.boxTextColor,
+          ),
+        ),
+        iconTheme: IconThemeData(color: theviewModel.currentTheme.boxTextColor),
       ),
       body: Center(
         child: Column(
